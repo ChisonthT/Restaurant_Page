beb3c537d38dea4633001feec458acd8558c67a1
Chisonth <1533494+ChisonthT@users.noreply.github.com>
2025-02-28 15:39:30 -0500

    Completed step 5. A mock restaurant page works.

--- webpack.config.js
+++ webpack.config.js
@@ -18,5 +18,22 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
-  ]
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
